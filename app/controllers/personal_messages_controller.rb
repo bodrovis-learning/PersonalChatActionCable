9f33b5bcee854badcf025cebe42b0dcdd365e3fc
bodrovis <ibodrov@campaigner.ru>
2016-07-16 18:47:27 +0300

    redirect to a conversation if possible

--- app/controllers/personal_messages_controller.rb
+++ app/controllers/personal_messages_controller.rb
@@ -2,19 +2,19 @@ class PersonalMessagesController < ApplicationController
   before_action :check_receiver!, only: [:new, :create]
 
   def new
+    redirect_to conversation_path(@conversation) and return if @conversation
     @personal_message = current_user.personal_messages.build
   end
 
   def create
-    conversation = Conversation.between(current_user.id, @receiver.id)[0]
-    conversation ||= Conversation.create(author_id: current_user.id,
+    @conversation ||= Conversation.create(author_id: current_user.id,
                                          receiver_id: @receiver.id)
     @personal_message = current_user.personal_messages.build(personal_message_params)
     @personal_message.conversation_id = conversation.id
     @personal_message.save!
 
     flash[:success] = "ok!"
-    redirect_to conversation_path(conversation)
+    redirect_to conversation_path(@conversation)
   end
 
   private
@@ -25,6 +25,7 @@ class PersonalMessagesController < ApplicationController
 
   def check_receiver!
     @receiver = User.find_by(id: params[:receiver])
-    redirect_to root_path unless @receiver
+    redirect_to(root_path) and return unless @receiver
+    @conversation = Conversation.between(current_user.id, @receiver.id)[0]
   end
 end
